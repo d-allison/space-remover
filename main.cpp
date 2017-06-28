6b072f079223ff829d0c28e85c26a60023007895
d-allison <contact@danielallison.net>
2017-06-27 22:41:33 -0400

    Removed stuff

--- main.cpp
+++ main.cpp
@@ -20,11 +20,6 @@ int main(int argc, char *argv[])
     string outputFileName;
     outputFileName = filePath.substr(filePath.find_last_of("\\") + 1, filePath.find('.') - filePath.find_last_of("\\") - 1 ) + "_mod" + filePath.substr(filePath.find_last_of('.'), filePath.length() - filePath.find_last_of('.'));
 
-    cout << filePath << endl;
-    cout << filePath.find_last_of("\\") + 1 << endl;
-    cout << filePath.find('.') << endl;
-
-
     cout << outputFileName << endl;
 
     ifstream inputFile;
@@ -47,9 +42,6 @@ int main(int argc, char *argv[])
     }
     inputFile.close();
     outputFile.close();
-
-    int end;
-    cin >> end;
 
     return 0;
 }
